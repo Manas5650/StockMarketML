7a4628338910e578295ef58e829f922e1bd17c3c
Manas5650 <manas321gupta@gmail.com>
2025-09-30 18:41:40 +0530

    Final changes

--- app.py
+++ app.py
@@ -425,4 +425,5 @@ def upload_csv():
 
 # ---------------------------------------------------
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000, debug=False)
+    # app.run(host="127.0.0.1", port=5000, debug=False)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
